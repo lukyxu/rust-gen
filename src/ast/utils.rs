50fad916e729a9af94f65417183f332527a75a7a
Kerry Xu <kx3418@ic.ac.uk>
2022-05-23 04:03:36 +0100

    Add apply limit expr functions

--- src/ast/utils.rs
+++ src/ast/utils.rs
@@ -3,6 +3,7 @@ use crate::ast::item::ItemKind;
 use crate::ast::stmt::StmtKind;
 use crate::ast::ty::{Ty, TyKind};
 use crate::context::Context;
+use std::cmp::{min};
 use std::collections::HashMap;
 use std::hash::Hash;
 
@@ -29,6 +30,31 @@ limit_function!(limit_arith_depth, arith_depth, max_arith_depth);
 limit_function!(limit_expr_depth, expr_depth, max_expr_depth);
 limit_function!(limit_if_else_depth, if_else_depth, max_if_else_depth);
 limit_function!(limit_block_depth, block_depth, max_block_depth);
+
+macro_rules! apply_limit_function_composite {
+    ($function_name: ident, $max_depth: ident, $curr_depth: ident, $max_depth_delta: ident) => {
+        /// Wrapper function that controls various depths.
+        pub fn $function_name<T: 'static, S: 'static>(
+            f: fn (&mut Context, &S) -> Option<T>,
+            ctx: &mut Context,
+            res_type: &S
+        ) -> Option<T> {
+            let prev_max_expr_depth = ctx.policy.$max_depth;
+            ctx.policy.$max_depth = min(
+                ctx.policy.$max_depth,
+                ctx.$curr_depth + ctx.policy.$max_depth_delta,
+            );
+            let res = f(ctx, res_type);
+            ctx.policy.$max_depth = prev_max_expr_depth;
+            res
+        }
+    };
+}
+
+apply_limit_function_composite!(apply_limit_expr_depth_in_tuple, max_expr_depth, expr_depth, max_expr_depth_in_tuple);
+apply_limit_function_composite!(apply_limit_expr_depth_in_array, max_expr_depth, expr_depth, max_expr_depth_in_array);
+apply_limit_function_composite!(apply_limit_expr_depth_in_struct, max_expr_depth, expr_depth, max_expr_depth_in_struct);
+
 
 /// Increments statistic counters.
 pub fn increment_counter<T, K: Eq + Hash, S: ::std::hash::BuildHasher>(
